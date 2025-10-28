9d13c8cb265f7f8c6c6f40fc2cd2a8ebfbe7ed89
AdityaP183 <adityaprasad1828@gmail.com>
2025-10-28 21:08:16 +0530

    feat(db): integrate Prisma ORM with PostgreSQL, add schema and PrismaService setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,28 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { AuthModule } from './modules/auth/auth.module';
-import { UserModule } from './modules/user/user.module';
-import { SongsModule } from './modules/songs/songs.module';
-import { ArtistsModule } from './modules/artists/artists.module';
-import { AlbumsModule } from './modules/albums/albums.module';
-import { GenresModule } from './modules/genres/genres.module';
+
+import { AuthModule } from "./modules/auth/auth.module";
+import { UserModule } from "./modules/user/user.module";
+import { SongsModule } from "./modules/songs/songs.module";
+import { ArtistsModule } from "./modules/artists/artists.module";
+import { AlbumsModule } from "./modules/albums/albums.module";
+import { GenresModule } from "./modules/genres/genres.module";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
     controllers: [AppController],
     providers: [AppService],
-    imports: [AuthModule, UserModule, SongsModule, ArtistsModule, AlbumsModule, GenresModule],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
+        AuthModule,
+        UserModule,
+        SongsModule,
+        ArtistsModule,
+        AlbumsModule,
+        GenresModule,
+    ],
 })
 export class AppModule {}
